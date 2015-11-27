e214ca0f3a361f49eb17598c33f2f6cbb525f4bd
Cedric Beust <cedric@beust.com>
2015-11-27 10:59:39 -0800

    Signing configs.

--- android-flavors/Build.kt
+++ android-flavors/Build.kt
@@ -2,7 +2,7 @@ import com.beust.kobalt.*
 import com.beust.kobalt.api.*
 import com.beust.kobalt.plugin.apt.*
 import com.beust.kobalt.plugin.packaging.assemble
-import com.beust.kobalt.plugin.android.android
+import com.beust.kobalt.plugin.android.*
 import com.beust.kobalt.plugin.java.*
 
 val p = javaProject {
@@ -16,6 +16,13 @@ val p = javaProject {
         compileSdkVersion = "23"
         buildToolsVersion = "23.0.1"
         applicationId = "com.beust.kobalt.android.helloworld"
+
+        signingConfig("release") {
+            storeFile = "other.keystore"
+            storePassword = "android"
+            keyAlias = "androiddebugkey"
+            keyPassword = "android"
+        }
     }
 
     productFlavor("free") {
