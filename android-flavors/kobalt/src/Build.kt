5305759281a0e0d898c5e25f8db7097d229e7179
Cedric Beust <cedric@beust.com>
2016-03-04 01:08:37 +0400

    Merging

--- android-flavors/kobalt/src/Build.kt
+++ android-flavors/kobalt/src/Build.kt
@@ -1,156 +1,67 @@
-
 import com.beust.kobalt.*
 import com.beust.kobalt.api.*
-import com.beust.kobalt.api.annotation.Task
-import com.beust.kobalt.plugin.application.application
-import com.beust.kobalt.plugin.dokka.*
-import com.beust.kobalt.plugin.java.javaCompiler
-import com.beust.kobalt.plugin.java.javaProject
-import com.beust.kobalt.plugin.kotlin.kotlinCompiler
-import com.beust.kobalt.plugin.kotlin.project
+import com.beust.kobalt.plugin.apt.*
 import com.beust.kobalt.plugin.packaging.assemble
-import com.beust.kobalt.plugin.publish.*
-import com.beust.kobalt.test
-import java.io.File
-import java.nio.file.Files
-import java.nio.file.Paths
-import java.nio.file.StandardCopyOption
+import com.beust.kobalt.plugin.android.*
+import com.beust.kobalt.plugin.java.*
 
-val r = repos("http://dl.bintray.com/kotlin/kotlinx.dom")
+//val pl = plugins("com.beust:kobalt-android:0.43")
+val pl = plugins(file(homeDir("kotlin/kobalt-android/kobaltBuild/libs/kobalt-android-0.44.jar")))
 
-val wrapper = project {
-    name = "kobalt-wrapper"
-    version = readVersion()
-    directory = "modules/wrapper"
+val p = project {
 
-    javaCompiler {
-        args("-source", "1.7", "-target", "1.7")
+    name = "kobalt.helloworld"
+    group = "com.example"
+    artifactId = name
+    version = "0.1"
+
+    android {
+        compileSdkVersion = "23"
+        buildToolsVersion = "23.0.1"
+        applicationId = "com.beust.kobalt.android.helloworld"
+
+//        signingConfig("release") {
+//            storeFile = homeDir(".android", "debug.keystore")
+//            storePassword = "android"
+//            keyAlias = "androiddebugkey"
+//            keyPassword = "android"
+//        }
     }
 
-    assemble {
-        jar {
-            name = projectName + ".jar"
-            manifest {
-                attributes("Main-Class", "com.beust.kobalt.wrapper.Main")
-            }
+    productFlavor("free") {
+        buildConfig {
+            field("String", "freeField", "\"The free field\"")
         }
     }
 
-//    productFlavor("dev") {
-//    }
-//
-//    buildType("debug") {
-//    }
-//
-    application {
-        mainClass = "com.beust.kobalt.wrapper.Main"
+    productFlavor("pro") {
+        buildConfig {
+            field("String", "proField", "\"The pro field\"")
+        }
     }
-}
 
-val kobalt = project(wrapper) {
-    name = "kobalt"
-    group = "com.beust"
-    artifactId = name
-    version = readVersion()
-    description = "A build system in Kotlin"
-    url = "http://beust.com/kobalt"
-    licenses = arrayListOf(License("Apache 2.0", "http://www.apache.org/licenses/LICENSE-2.0"))
-    scm = Scm(url = "http://github.com/cbeust/kobalt",
-            connection = "https://github.com/cbeust/kobalt.git",
-            developerConnection = "git@github.com:cbeust/kobalt.git")
+    buildType("debug") {
+        buildConfig {
+            field("String", "debugField", "\"The debug field\"")
+        }
+    }
 
-    dependenciesTest {
-        compile("org.testng:testng:6.9.9")
+    buildType("release") {
+        minifyEnabled = true
+//        proguardFile = getDefaultProguardFile("proguard-android.txt")
+
+        buildConfig {
+            field("String", "releaseField", "\"The release field\"")
+        }
     }
 
     dependencies {
-        compile("org.jetbrains.kotlin:kotlin-stdlib:1.0.0-beta-2423",
-                "org.jetbrains.kotlin:kotlin-compiler-embeddable:1.0.0-beta-2423",
-                "org.jetbrains.dokka:dokka-fatjar:0.9.2",
-                "org.jetbrains.kotlinx:kotlinx.dom:0.0.2",
-
-                "com.beust:jcommander:1.48",
-                "com.squareup.okhttp:okhttp:2.5.0",
-                "org.jsoup:jsoup:1.8.3",
-                "com.google.inject:guice:4.0",
-                "com.google.inject.extensions:guice-assistedinject:4.0",
-                "javax.inject:javax.inject:1",
-                "com.google.guava:guava:19.0-rc2",
-                "org.apache.maven:maven-model:3.3.3",
-                "com.github.spullara.mustache.java:compiler:0.9.1",
-                "io.reactivex:rxjava:1.0.16",
-                "com.google.code.gson:gson:2.4",
-                "com.squareup.retrofit:retrofit:1.9.0",
-                "com.squareup.okio:okio:1.6.0"
-              )
+        compile("com.android.support:appcompat-v7:23.0.1"
+                ,"com.android.support:design:23.0.1"
+        )
     }
 
-
-    assemble {
-        mavenJars {
-            fatJar = true
-            manifest {
-                attributes("Main-Class", "com.beust.kobalt.MainKt")
-            }
-        }
-        zip {
-            include("kobaltw")
-            include(from("$buildDirectory/libs"), to("kobalt/wrapper"),
-                    "$projectName-$version.jar")
-            include(from("modules/wrapper/$buildDirectory/libs"), to("kobalt/wrapper"),
-                    "$projectName-wrapper.jar")
-        }
+    javaCompiler {
+        args("-source", "1.6", "-target", "1.6")
     }
-
-//    install {
-//        libDir = "lib-test"
-//    }
-
-    test {
-        args("-log", "1", "src/test/resources/testng.xml")
-    }
-
-    kotlinCompiler {
-        args("-nowarn")
-    }
-
-    dokka {
-        outputFormat = "markdown"
-    }
-
-    dokka {
-        outputFormat = "html"
-    }
-
-    github {
-        file("$buildDirectory/libs/$name-$version.zip", "$name/$version/$name-$version.zip")
-    }
-
-    bintray {
-        publish = true
-    }
-}
-
-fun readVersion() : String {
-    val p = java.util.Properties()
-    var localFile = java.io.File("src/main/resources/kobalt.properties")
-    if (! localFile.exists()) {
-        localFile = File(homeDir("kotlin", "kobalt", "src/main/resources/kobalt.properties"))
-    }
-    p.load(java.io.FileReader(localFile))
-    return p.getProperty("kobalt.version")
-}
-
-@Task(name = "copyVersionForWrapper", runBefore = arrayOf("assemble"), runAfter = arrayOf("compile"), description = "")
-fun taskCopyVersionForWrapper(project: Project) : TaskResult {
-    if (project.name == "kobalt-wrapper") {
-        val toString = "modules/wrapper/kobaltBuild/classes"
-        File(toString).mkdirs()
-        val from = Paths.get("src/main/resources/kobalt.properties")
-        val to = Paths.get("$toString/kobalt.properties")
-        Files.copy(from,
-                to,
-                StandardCopyOption.REPLACE_EXISTING)
-    }
-    return TaskResult()
 }
--- android/kobalt/src/Build.kt
+++ android/kobalt/src/Build.kt
@@ -3,8 +3,10 @@ import com.beust.kobalt.plugin.packaging.assemble
 import com.beust.kobalt.plugin.android.*
 import com.beust.kobalt.plugin.java.*
 
-//val pl = plugins("com.beust:kobalt-android:0.23")
-val pl = plugins(file(homeDir("kotlin/kobalt-android/kobaltBuild/libs/kobalt-android-0.26.jar")))
+val pl = plugins(
+    "com.beust:kobalt-android:0.43"
+//    file(homeDir("kotlin/kobalt-android/kobaltBuild/libs/kobalt-android-0.43.jar"))
+    )
 
 val p = project {
 
@@ -12,6 +14,7 @@ val p = project {
     group = "com.example"
     artifactId = name
     version = "0.1"
+    directory = "app"
 
     android {
         compileSdkVersion = "17"
@@ -20,7 +23,11 @@ val p = project {
     }
 
     dependencies {
+<<<<<<< HEAD
         compile(file("libs/android-async-http-1.4.3.jar"),
+=======
+        compile(file("app/libs/android-async-http-1.4.3.jar"),
+>>>>>>> Update.
                 "com.android.support:support-v4:21.0.3")
     }
 
